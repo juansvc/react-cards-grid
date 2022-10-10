c58933a6b37bb8f5a6aaf4a48639db65b196e3cb
Juan Vivanco <juansebastianvc@gmail.com>
2022-10-10 17:39:01 -0500

    tags and 3d flip

--- app/src/components/DataCard/DataCard.js
+++ app/src/components/DataCard/DataCard.js
@@ -3,17 +3,32 @@ import React from 'react';
 const DataCard = ({ card }) => {
   return (
     <div className='card'>
-      <div className='card__image' role='img' aria-label='Card Img'>
-        <img src={card.image} alt='Product Img' loading='lazy' />
+      <div className='card__face card__face--front'>
+        <div className='card__image' role='img' aria-label='Card Img'>
+          <img src={card.image} alt='Product Img' loading='lazy' />
+        </div>
+        <div className='content'>
+          <h2 className='content__title'>
+            {/* Check title after colon */}
+            {card.title.indexOf(':') !== -1
+              ? card.title.split(':')[1]
+              : card.title}
+          </h2>
+          <p className='content__description--truncated'>{card.description}</p>
+        </div>
       </div>
-      <div className='content'>
-        <h2 className='content__title'>
-          {/* Check title after colon */}
-          {card.title.indexOf(':') !== -1
-            ? card.title.split(':')[1]
-            : card.title}
-        </h2>
-        <p className='content__description--truncated'>{card.description}</p>
+      <div className='card__face card__face--back'>
+        <div className='card__image' role='img' aria-label='Card Img'>
+          <img src={card.image} alt='Product Img' loading='lazy' />
+        </div>
+        <div className='content'>
+          <h2 className='content__title'>{card.id}</h2>
+          {/* <div className='tag'> */}
+          {card.tags.map((tag) => (
+            <span className='tag'>{tag}</span>
+          ))}
+          {/* </div> */}
+        </div>
       </div>
     </div>
   );
